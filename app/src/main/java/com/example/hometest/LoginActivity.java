bdc2cb7eaa0aacd4fe98fafb074a49e543c478fd
晓波 <2465916612@qq.com>
2017-10-18 16:45:46 +0800

    text

---
+++ app/src/main/java/com/example/hometest/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.hometest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 晓波 on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
